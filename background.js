76cd8e05934380ef36decbed13976c990fbc9b23
Raymond Zhao <raymond.zhao@datadoghq.com>
2022-03-31 15:38:30 -0400

    new tab

--- background.js
+++ background.js
@@ -1,4 +1,13 @@
 // background.js
+chrome.tabs.onCreated.addListener((tab) => {
+  if (1) {
+      chrome.tabs.update({
+        url: chrome.extension.getURL("index.html")
+      });
+      console.log("here lmao");
+  }
+  console.log("tab created", tab.url, tab);
+})
 
 let color = '#3aa757';
 
@@ -6,3 +15,5 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+
---
+++ check.js
@@ -0,0 +1,17 @@
+// console.log("here")
+
+// window.addEventListener('offline', () => 
+// console.log('Became offline')
+// );
+
+
+
+// chrome.tabs.onCreated.addListener((tab) => {
+//     if (1) {
+//         chrome.tabs.update({
+//           url: chrome.extension.getURL("index.html")
+//         });
+//         console.log("here lmao");
+//     }
+//     console.log("tab created", tab.url, tab);
+//   })
